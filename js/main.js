517831e01e0ed9738e5fd171cc7cb7db6ab826b0
Somar Ani <somaraani>
2018-10-18 17:34:32 -0400

    Changef boxlife to Cryptex

--- js/main.js
+++ js/main.js
@@ -1,75 +1,33 @@
 //Typing ----------------------------------------------------------------------------
 
-var string1 = "Somar Ani";
-var string2 = "Somar Aani"
-var timer;
+var string1 = "Somar Ani--ani";
 
-function typeName(i){
+function type(i, str){
     
-    if(i == string1.length + 1){
-        eraseName(0);
-    }
-    else{
-        
-        if(i == 6){
-            document.getElementById("typingText").innerHTML = string1.substring(0,i) + '&nbsp;' + '<span id="span" class="blink" aria-hidden="true"></span>';
-        }
-        
-        else if(i <= string1.length){
-            document.getElementById("typingText").innerHTML = string1.substring(0,i) + '<span  id="span" class="blink" aria-hidden="true"></span>' ;
-        }
-
-        setTimeout(function() {
-            typeName(i+1);
-        }, 300);
-    }
-}
-
-function eraseName(i){
-    
-    if(i == 3){
-        reWriteName(7);
-    }
-    else{
-        document.getElementById("typingText").innerHTML = string1.substring(0,string1.length - i) + '<span id="span" class="blink" aria-hidden="true"></span>' ;
-        
-        i++;
-    
-        setTimeout(function() {
-            eraseName(i);
-            }, 300);
-    }
-}
-
-function reWriteName(i){
-    
-    if(i == 69){
-      document.getElementById("span").className = "noblink";
-    
-      clearTimeout(timer);
-    }
-    else{
-    
-    if(i == string2.length+1){
-        i = 69;
-        
-        setTimeout(function() {
-            reWriteName(i);
+    //removes blink and exits function at end of string
+    if(i == str.length){
+        setTimeout(function() { 
+              $("#span").addClass('noblink');
         }, 2600);
+        return;
     }
-    else{
-    document.getElementById("typingText").innerHTML = string2.substring(0,i) + '<span id="span" class="blink" aria-hidden="true"></span>' ;
     
-    i++; 
+    //deletes last character at '-'
+    if (str.charAt(i) == '-') {
+        setTimeout(function() { type(i - 2, str.substring(0,i-1) + str.substring(i + 1)); }, 175);
+        return;
+    }
     
-    setTimeout(function() {
-            reWriteName(i);
-        }, 300);
+    var space = "";
+    if(str.charAt(i) == ' ') {
+        space = '&nbsp;';
     }
-    }
- }
+    
+    $('#typingText').html(str.substring(0,i + 1) + space + '<span id="span" class="blink" aria-hidden="true"></span>');
+    setTimeout(function() { type(i+1, str); }, 250);
+}
 
-// Nav ---------------------------------------------------------------------------------
+// Side nav  ---------------------------------------------------------------------------------
 
 function openNav(a) {
     var width = 650;
@@ -78,27 +36,38 @@ function openNav(a) {
     }
 
     $('#sidenav').html("");
-
+    
     $('#sidenav').css('width', width + "px");
     $('#main').css('margin-right',width + "px");
     $('#dimmer').css('display', 'block');
-
+    
     $('#sidenav').load(a + ".html");
 }
 
 function closeNav() {
-    
-    var x = document.getElementsByClassName("sidenav");
-    var i;
-    for(i = 0; i < x.length; i++)
-        x[i].style.width = "0";
-    
-    document.getElementById("main").style.marginRight = "0";
-    document.getElementById("dimmer").style.display='none';
+    $('#sidenav').css('width', 0 + "px");
+    $("#main").css('margin-right', 0);
+    $('#dimmer').css('display', 'none');
 }
+
+// Top nav  ---------------------------------------------------------------------------------
 
 var navbar = $('#navbar');
 var offset = 400;
+
+window.onscroll = function(){    
+    if(window.pageYOffset > offset){
+        navbar.addClass('sticky');
+    }
+    else{
+        navbar.removeClass('sticky');
+    }
+
+    var scrollpct = Math.floor($(window).scrollTop()/($(document).height() - $(window).height()) * 100) // gets percentage scrolled (ie: 80 NaN if tracklength == 0)
+    $('div#navprog').width(scrollpct + '%');
+};
+
+// Window load  ---------------------------------------------------------------------------------
  
 $(window).ready(updateHeight);
 $(window).resize(updateHeight);
@@ -106,12 +75,9 @@ $(window).resize(updateHeight);
 $(window).load(function() {
     scrollToTop();
     $("#cover").delay(2000).slideUp(1500);
-    setTimeout(function() {typeName(0)}, 3500);
+    setTimeout(function() {type(0, string1)}, 3500);
     setTimeout(function() {$("#headerImg").addClass("imgSlide");  $("#headerImg").removeClass("imgHidden");}, 2500);
-    setTimeout(function() {$("#great").addClass("bodySlide");}, 1600);
-    
-    //setTimeout(function(){loaded();}, 2000);
-    
+    setTimeout(function() {$("#great").addClass("bodySlide");}, 1600);    
 });
 
 function scrollToTop(){
@@ -129,18 +95,6 @@ function updateHeight(){
        $('#loader').css('width',height);
    }
 }
-
-window.onscroll = function(){    
-    if(window.pageYOffset > offset){
-        navbar.addClass('sticky');
-    }
-    else{
-        navbar.removeClass('sticky');
-    }
-
-    var width = 120 * $(window).scrollTop() / $(document).height()
-    $('div#navprog').width(width + '%');
-};
 
 $("#aboutlink").click(function() {
     $('html, body').animate({
